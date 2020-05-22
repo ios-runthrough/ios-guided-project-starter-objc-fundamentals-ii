2f5d384573a29cece2f28bc7e9ead978a817c68b
Alex Shillingford <alex.shillingford@icloud.com>
2020-05-21 19:52:40 -0600

    Add model file to the project

---
+++ Tips/Models/LSITip.m
@@ -0,0 +1,13 @@
+//
+//  LSITip.m
+//  Tips
+//
+//  Created by Alex Shillingford on 5/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "LSITip.h"
+
+@implementation LSITip
+
+@end
