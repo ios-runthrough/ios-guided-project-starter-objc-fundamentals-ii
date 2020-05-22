e7daffd0ee8ad3b266b3c804450291220c9e10c7
Alex Shillingford <alex.shillingford@icloud.com>
2020-05-21 20:22:04 -0600

    Add Model Controller File

---
+++ Tips/Model Controllers/LSITipController.h
@@ -0,0 +1,17 @@
+//
+//  LSITipController.h
+//  Tips
+//
+//  Created by Alex Shillingford on 5/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSITipController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tips/Model Controllers/LSITipController.m
@@ -0,0 +1,13 @@
+//
+//  LSITipController.m
+//  Tips
+//
+//  Created by Alex Shillingford on 5/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "LSITipController.h"
+
+@implementation LSITipController
+
+@end
